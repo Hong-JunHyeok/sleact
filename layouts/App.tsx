9637b9adee603d43a9698d59454fe069c2446e34
HongJunHyeok <edb1631@naver.com>
2021-06-05 19:45:04 +0900

    ADD code spliting using loadable component

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,14 +1,18 @@
-import Login from '@pages/Login';
-import SingUp from '@pages/SingUp';
-import React from 'react';
+import React, { FC } from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
 
-const App = () => {
+// import Login from '@pages/Login';
+// import SingUp from '@pages/SingUp';
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App: FC = () => {
   return (
     <Switch>
       <Redirect exact path="/" to="/login"></Redirect>
       <Route path="/login" component={Login} />
-      <Route path="/signup" component={SingUp} />
+      <Route path="/signup" component={SignUp} />
     </Switch>
   );
 };
