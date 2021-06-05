782ace2df4468946d8d250861a8f9aa23e874c42
HongJunHyeok <edb1631@naver.com>
2021-06-05 19:28:26 +0900

    ADD Login SignUp routing

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,7 +1,16 @@
+import Login from '@pages/Login';
+import SingUp from '@pages/SingUp';
 import React from 'react';
+import { Switch, Redirect, Route } from 'react-router-dom';
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login"></Redirect>
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SingUp} />
+    </Switch>
+  );
 };
 
 export default App;
---
+++ pages/SingUp/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SingUp = () => {
+  return <>회원가입</>;
+};
+
+export default SingUp;
