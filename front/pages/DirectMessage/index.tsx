becf16015fb988544fb3319e83c56dfde577c729
HongJunHyeok <edb1631@naver.com>
2021-06-19 22:47:26 +0900

    ADD mutating chat

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -36,7 +36,20 @@ const DirectMessage = () => {
     (e) => {
       e.preventDefault();
 
-      if (chat?.trim()) {
+      if (chat?.trim() && chatData) {
+        const savedChat = chat;
+        mutateChat((prevChatData) => {
+          prevChatData?.[0].unshift({
+            id: (chatData[0][0]?.id || 0) + 1,
+            content: savedChat,
+            SenderId: myData!.id,
+            Sender: myData!,
+            ReceiverId: userData!.id,
+            Receiver: userData!,
+            createdAt: new Date(),
+          });
+          return prevChatData;
+        }, false);
         axios
           .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
             content: chat,
